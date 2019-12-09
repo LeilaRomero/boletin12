8d74f82fc118edcaec999bf9fd4acbe8d33d3712
Leila <Leila@DESKTOP-LU1NCHG>
2019-12-09 17:42:14 +0100

    Bol5

---
+++ Bol12_1/src/bol12_1/Bol12_1.java
@@ -0,0 +1,19 @@
+package bol12_1;
+
+/**
+ *
+ * @author Leila
+ */
+public class Bol12_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Garaje obj = new Garaje();
+        obj.programa();
+
+    }
+
+}
